338138709157e6cb171ace54be06ef3d22ea164c
*pknowledge* <mishrasangeeta944@gmail.com>
2023-12-10 22:05:08 +0530

    tailwin css

--- do/src/App.js
+++ do/src/App.js
@@ -10,8 +10,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Hello world</h2>
         </div>
+        
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+                                                                                                                
         </p>
       </div>
     );
